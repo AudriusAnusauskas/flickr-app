b550f908c8ea1fe94876cdde666e4a1e50b1c762
AudriusAnusauskas <aaaudrius@gmail.com>
2023-03-19 12:39:42 +0200

    move photos fetching to separate service

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { getPhotos } from "../../services/photo.service";
 import Photo from "../Photo/Photo";
 
 export interface FlickrPhoto {
@@ -9,22 +10,13 @@ export interface FlickrPhoto {
   title: string;
 }
 
-const API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
-
 const Gallery: React.FC = () => {
   const [photos, setPhotos] = useState<FlickrPhoto[]>([]);
 
   useEffect(() => {
     const fetchPhotos = async () => {
-      try {
-        const response = await fetch(
-          `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=rockbands&per_page=30&page=1&format=json&nojsoncallback=1`
-        );
-        const data = await response.json();
-        setPhotos(data.photos.photo);
-      } catch (error) {
-        console.log(error);
-      }
+      const newPhotos = await getPhotos("rockconcert");
+      setPhotos(newPhotos);
     };
     fetchPhotos();
   }, []);
