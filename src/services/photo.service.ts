63b5217b6b3a46bd0c39204d1b91ead9a77f5e1d
AudriusAnusauskas <aaaudrius@gmail.com>
2023-03-19 13:45:22 +0200

    implement Loader component, update gallery with it

--- src/services/photo.service.ts
+++ src/services/photo.service.ts
@@ -5,7 +5,7 @@ const API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
 export const getPhotos = async (tags: string): Promise<FlickrPhoto[]> => {
   try {
     const response = await fetch(
-      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${tags}&per_page=30&page=1&format=json&nojsoncallback=1`
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${tags}&per_page=35&page=1&format=json&nojsoncallback=1`
     );
     const data = await response.json();
     return data.photos.photo;
