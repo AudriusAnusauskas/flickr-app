3c419fda42ece48104055946593e9ce9f4993028
AudriusAnusauskas <aaaudrius@gmail.com>
2023-03-26 12:42:01 +0300

    implement Favourites component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Gallery from "../Gallery/Gallery";
+import Favourites from "../Favourites/Favourites";
 
 import style from "./Layout.module.css";
 
 const Layout: React.FC = () => {
+  const [showFavourites, setShowFavourites] = useState(false);
+
+  const toggleFavourites = () => {
+    setShowFavourites(!showFavourites);
+  };
+
   return (
     <div className={style.layout} data-testid="gallery">
-      <Gallery />
+      <header className={style.header}>
+        <button onClick={toggleFavourites} className={style.favButton}>
+          {showFavourites ? "Show Gallery" : "Show Favourites"}
+        </button>
+      </header>
+      {!showFavourites ? <Gallery /> : <Favourites />}
     </div>
   );
 };
